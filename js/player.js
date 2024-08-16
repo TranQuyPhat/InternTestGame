d4a0160ca893bcbd284d3de2ea0630e2bc8bd6ce
q_phat <Quyphat.162@gmail.com>
2024-08-16 15:57:33 +0700

    double jump function

--- js/player.js
+++ js/player.js
@@ -7,33 +7,38 @@ game.player = {
   isInAir: false,
   startedJump: false,
   moveInterval: null,
-  fallTimeout: function (startingY, time, maxHeight) {
-    setTimeout(
+  fallTimeout: function (startingY, time, maxHeight, count) {
+    return setTimeout(
       function () {
-        if (this.isInAir) {
-          this.y = startingY - maxHeight + Math.pow(-time / 3 + 11, 2);
-          if (this.y < this.highestY) {
-            this.highestY = this.y;
+        if (!(count == 1 && this.doubleJump)) {
+          if (this.isInAir) {
+            this.y = startingY - maxHeight + Math.pow(-time / 3 + 11, 2);
+
+            if (this.y < this.highestY) {
+              this.highestY = this.y;
+            }
+            if (time > 37) {
+              this.startedJump = false;
+              this.doubleJump = false;
+              game.checkCollisions();
+            }
+            if (time < 150) {
+              time++;
+              this.fallTimeout(startingY, time, maxHeight, count);
+            } else {
+              game.isOver = true;
+            }
+            if (this.y > 40) {
+              game.isOver = true;
+            }
+            game.requestRedraw();
           }
-          if (time > 37) {
-            this.startedJump = false;
-            game.checkCollisions();
-          }
-          if (time < 150) {
-            time++;
-            this.fallTimeout(startingY, time, maxHeight);
-          } else {
-            game.isOver = true;
-          }
-          if (this.y > 40) {
-            game.isOver = true;
-          }
-          game.requestRedraw();
         }
-      }.bind(this, startingY, time, maxHeight),
+      }.bind(this, startingY, time, maxHeight, count),
       12
     );
   },
+
   animationFrameNumber: 0,
   collidesWithGround: true,
   animations: {
@@ -52,20 +57,23 @@ game.player = {
     ],
   },
 
-  jump: function (type) {
-    if (!this.isInAir) {
-      clearInterval(this.fallInterval);
+  jump: function (type, count) {
+    console.log(this.doubleJump);
+
+    if (!this.isInAir || (this.doubleJump && count != 0)) {
       game.sounds.jump.play();
       this.isInAir = true;
       this.startedJump = true;
       var startingY = this.y;
+      console.log(`:: STARTING Y : ${startingY}`);
+
       var time = 1;
       maxHeight = 121;
       if (type == "fall") {
         time = 30;
         maxHeight = 0;
       }
-      this.fallTimeout(startingY, time, maxHeight);
+      return this.fallTimeout(startingY, time, maxHeight, count);
     }
   },
 };
